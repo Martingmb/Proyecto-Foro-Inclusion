9e8ca1eea535de0f04f18bb39ec93ac5fa6469e2
Martín Moreno <martingmb@hotmail.com>
2019-03-25 10:19:06 -0600

    Setup of Project

---
+++ ProyectoFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoFinal
+//
+//  Created by Martín on 3/25/19.
+//  Copyright © 2019 Martín Gerardo Moreno Barrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
